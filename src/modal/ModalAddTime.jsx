69c9739de54095a41ff2735ca436a14ed6cce5dd
FADHILLA DWI PRAMESWARY RAYES <fadhilla.dwi.p.r@gmail.com>
2021-08-17 13:32:00 +0700

    modaladdtime fix sementara

--- src/modal/ModalAddTime.jsx
+++ src/modal/ModalAddTime.jsx
@@ -9,40 +9,42 @@ function TimeModal({ closeModal }) {
   const [value, onChange] = useState(new Date());
 
   return (
-    <div className="time__container">
-      <div className="time__wrapper">
-        <div className="time__title">
-          <h1>Pick Date</h1>
-        </div>
-        <div className="time__input">
-          <div className="time__date">
-            <h3>Date</h3>
-            <input type="text" className="input-time" id="time" />
+    <div className="time__outside">
+      <div className="time__container">
+        <div className="time__wrapper">
+          <div className="time__title">
+            <h1>Pick Date</h1>
           </div>
-          <div className="time__time">
-            <h3>Time</h3>
-            <input type="text" className="input-time" id="time" />
+          <div className="time__input">
+            <div className="time__date">
+              <h3>Date</h3>
+              <input type="text" className="input-time" id="time" />
+            </div>
+            <div className="time__time">
+              <h3>Time</h3>
+              <input type="text" className="input-time" id="time" />
+            </div>
+          </div>
+          <div className="time__calendar">
+            <Calendar onChange={onChange} value={value} />
           </div>
         </div>
-        <div className="time__calendar">
-          <Calendar onChange={onChange} value={value} />
-        </div>
-      </div>
-      <div className="time__button">
-        <button className="notification">
-          <img src={buttonNotifLogo} alt="" />
-          <p>Notification</p>
-        </button>
-        <div className="time__button2">
-          <button className="time__save">Save Note</button>
-          <button
-            className="time__cancel"
-            onClick={() => {
-              closeModal(false);
-            }}
-          >
-            Cancel
+        <div className="time__button">
+          <button className="notification">
+            <img src={buttonNotifLogo} alt="" />
+            <p>Notification</p>
           </button>
+          <div className="time__button2">
+            <button className="time__save">Save Note</button>
+            <button
+              className="time__cancel"
+              onClick={() => {
+                closeModal(false);
+              }}
+            >
+              Cancel
+            </button>
+          </div>
         </div>
       </div>
     </div>
