fa44ee8eb76e24a7a36bb1b65d0679504c62609c
Aulia Sutowijoyo <aulia.sutowijoyo@gmail.com>
2021-08-22 22:02:45 +0700

    fix merge

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 import Router from './routes/router';
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import './Custom.scss'
 // import NewUser from "./Pages/CreateTask/NewUser";
-=======
-import router from "./routes/router";
-import { BrowserRouter } from "react-router-dom";
-import NewUser from "./Pages/CreateTask/NewUser";
->>>>>>> e5ac9f178374e194290205b3930d3321b0803cd5
 // import ModalTask from "./modal/ModalTask";
 // import ModalNotes from "./modal/ModalNotes";
 import ModalAddTime from "./modal/ModalAddTime";
@@ -32,7 +26,7 @@ function App() {
         {/* <ModalNotes /> */}
         {/* <ModalAddTime />
         <TaskModal /> */}
-        <NewUser />
+        {/* <NewUser /> */}
         {/* <ModalAddTime /> */}
         {/* <TaskModal /> */}
         {/* <ModalNote /> */}
