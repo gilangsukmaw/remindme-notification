171ceee43cafa7cc85090e899b085a9f7280480e
Aulia Sutowijoyo <aulia.sutowijoyo@gmail.com>
2021-08-25 11:10:06 +0700

    all goals add progress modal done

--- src/Pages/GoalsPage/AllGoals.jsx
+++ src/Pages/GoalsPage/AllGoals.jsx
@@ -5,22 +5,25 @@ import Garis from '../../assets/images/GoalDetailLine.png'
 import DetailCircular from '../../component/CircularGoals/DetailCircular';
 
 function AllGoals() {
-    return (
-        <>
-        <div className='GoalsContainer d-flex overflow-auto mt-5' style={{maxWidth:'1440px', margin:'2rem' }}>
-            
-            <CircularGoals/>
-                
-        </div>
-        <br/>
-        <div>
-            <img style={{width:'100%', height:'15px', padding:'0 2rem 0 2rem'}} src={Garis}></img>
-        </div>
-        <div>
-        <DetailCircular/>
-        </div>
-        </>
-    )
+return (
+<>
+<div className='PageTitle' style={{ margin:'2rem', }}>
+<p style={{fontSize:'34px', fontWeight:'500' }}>My All Goals</p>
+</div>
+    <div className='GoalsContainer d-flex overflow-auto mt-5' style={{maxWidth:'1440px', marginLeft:'2rem' }}>
+
+        <CircularGoals />
+
+    </div>
+    <br />
+    <div>
+        <img style={{width:'100%', height:'15px', padding:'0 2rem 0 2rem'}} src={Garis}></img>
+    </div>
+    <div>
+        <DetailCircular />
+    </div>
+</>
+)
 }
 
 export default AllGoals
