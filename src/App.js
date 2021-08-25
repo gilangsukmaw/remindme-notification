a9db8676057e3fb104c8f1d27e562b4240821c0b
FADHILLA DWI PRAMESWARY RAYES <fadhilla.dwi.p.r@gmail.com>
2021-08-26 05:51:03 +0700

    cek aja

--- src/App.js
+++ src/App.js
@@ -1,41 +1,21 @@
-import Router from './routes/router';
-import { BrowserRouter } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import './Custom.scss'
-// import NewUser from "./Pages/CreateTask/NewUser";
-// import ModalTask from "./modal/ModalTask";
-// import ModalNotes from "./modal/ModalNotes";
-import ModalAddTime from "./modal/ModalAddTime";
-import TaskModal from "./modal/ModalTask";
-import ModalNote from "./modal/ModalNote";
-import ModalDetailNote from "./modal/ModalDetailNote";
-import SaveChanges from "./modal/ModalSaveChanges";
-import Delete from "./modal/ModalDelete";
-import CreateTask from "./modal/CreateTask";
-
-
+import Router from "./routes/router";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import "./Custom.scss";
+import NotesPinnedCard from "./component/NotesCard/NotesCardPinned";
+import NotesUnPinnedCard from "./component/NotesCard/NotesUnpinned";
 
 function App() {
   return (
     <>
-    <div >
-      <BrowserRouter>
-        <Router />
-        {/* <NewUser /> */}
-        {/* <ModalNotes /> */}
-        {/* <ModalAddTime />
-        <TaskModal /> */}
-        {/* <NewUser /> */}
-        {/* <ModalAddTime /> */}
-        {/* <TaskModal /> */}
-        {/* <ModalNote /> */}
-        {/* <CreateTask />
-        <ModalDetailNote />
-        <SaveChanges />
-        <Delete /> */}
-       </BrowserRouter>
-    </div>
+      <div>
+        <BrowserRouter>
+          <Router />
+          <NotesPinnedCard />
+          <NotesUnPinnedCard />
+        </BrowserRouter>
+      </div>
     </>
   );
 }
