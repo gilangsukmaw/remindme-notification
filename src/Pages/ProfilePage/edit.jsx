b47e4dcf1b812635f3857ddfd52355f7008b682d
balyaGround <balyazulfikar@gmail.com>
2021-09-01 22:44:05 +0700

    masukin edit password

--- src/Pages/ProfilePage/edit.jsx
+++ src/Pages/ProfilePage/edit.jsx
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import pp from "../../assets/images/Ellipse 34.png";
 import edit from "../../assets/images/Ellipse 107.png";
 import "./editStyle.css";
-import { useParams } from "react-router-dom";
-import ModalEditPhoto from "./ModalEditPhoto";
+import axios from "axios";
 
-export default function Edit({ ...props }) {
-  const { type } = useParams();
+export default function Edit() {
+  const [user, setUser] = useState([]);
+
+  const Token = localStorage.getItem("Token");
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
+  const updateData = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.put(`https://remindme.gabatch13.my.id/api/v1/user`, update, { headers: { Authorization: `Bearer ${Token}` } });
+      alert("Profile Updated");
+      refreshPage();
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+  console.log("token", Token);
+
+  const getData = async () => {
+    await axios
+      .get(`https://remindme.gabatch13.my.id/api/v1/user/getinfo`, {
+        headers: { Authorization: `Bearer ${Token}` },
+      })
+      .then((result) => setUser(result?.data))
+      .catch((err) => console.log(err));
+  };
+
+  const [update, setUpdate] = useState({
+    firstname: "",
+    lastname: "",
+    username: "",
+    email: user.data ? user.data.email : null,
+    new_password: "",
+    confirm_password: "",
+  });
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log("update", update);
   return (
     <div>
       <div className="profile">
@@ -18,49 +57,61 @@ export default function Edit({ ...props }) {
           <div className="head">
             <img src={pp} alt="profile pictures" style={{ width: "230px" }} />
             <div className="edit-profile">
-              {type === "EditPhoto" ? <ModalEditPhoto /> : null}
+              {/* {type === "EditPhoto" ? <ModalEditPhoto /> : null} */}
               <img src={edit} alt="edit" />
             </div>
             <p style={{ marginTop: "-40px" }}>Edit Photo</p>
           </div>
           <div className="content-edit">
-            <form>
+            <form onSubmit={(e) => updateData(e)}>
               <div className="label">
                 <h5 htmlfor="" className="">
                   First Name
                 </h5>
-                <input />
+                <input onChange={(e) => setUpdate({ ...update, firstname: e.target.value })} />
               </div>
               <div className="label">
                 <h5 htmlfor="" className="">
                   Last Name
                 </h5>
-                <input />
+                <input onChange={(e) => setUpdate({ ...update, lastname: e.target.value })} />
               </div>
               <div className="label">
                 <h5 htmlfor="" className="">
                   Username
                 </h5>
-                <input />
+                <input onChange={(e) => setUpdate({ ...update, username: e.target.value })} />
               </div>
-              <div className="label">
+              <div className="label email">
                 <h5 htmlfor="" className="">
                   Email
                 </h5>
-                <p style={{ fontWeight: "Bold" }}>Amalianurlita97@gmail.com</p>
+                <input disabled style={{ fontWeight: "Bold" }} value={user.data ? user.data.email : null} onLoad={(e) => setUpdate({ ...update, email: e.target.value })} />
               </div>
               <div className="label password">
                 <h5 htmlfor="" className="">
-                  Password
+                  Old Password
                 </h5>
-                <input type="password" className="input-password" />
+                <input type="text" className="input-password" onChange={(e) => setUpdate({ ...update, new_password: e.target.value })} />
+              </div>
+              <div className="label password">
+                <h5 htmlfor="" className="">
+                  New Password
+                </h5>
+                <input type="text" className="input-password" onChange={(e) => setUpdate({ ...update, new_password: e.target.value })} />
+              </div>
+              <div className="label password">
+                <h5 htmlfor="" className="">
+                  Confirm Password
+                </h5>
+                <input type="text" className="input-password" onChange={(e) => setUpdate({ ...update, confirm_password: e.target.value })} />
               </div>
               <div>
-                <Link to="/profile">
-                  <button type="submit" className="btn-save">
-                    Save
-                  </button>
-                </Link>
+                {/* <Link to="/profile"> */}
+                <button type="submit" className="btn-save">
+                  Save
+                </button>
+                {/* </Link> */}
               </div>
             </form>
           </div>
